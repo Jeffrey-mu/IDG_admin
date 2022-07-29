ca77a5d976adfe1c75ab2a19eab1346e20279dbc
Jeffrey <1115378579@qq.com>
2022-07-29 21:43:50 +0800

    chore(vite): 修改vite配置

--- build/vite/proxy.ts
+++ build/vite/proxy.ts
@@ -19,7 +19,6 @@ export function createProxy(list: ProxyList = []) {
   const ret: ProxyTargetList = {};
   for (const [prefix, target] of list) {
     const isHttps = httpsRE.test(target);
-
     // https://github.com/http-party/node-http-proxy#options
     ret[prefix] = {
       target: target,
@@ -27,7 +26,7 @@ export function createProxy(list: ProxyList = []) {
       ws: true,
       rewrite: (path) => path.replace(new RegExp(`^${prefix}`), ''),
       // https is require secure=false
-      ...(isHttps ? { secure: false } : {}),
+      ...(isHttps ? { secure: true } : { secure: true }),
     };
   }
   return ret;
