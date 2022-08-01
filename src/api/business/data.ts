a1234c21dbc865aa622814856a2391d956df3e39
Jeffrey <1115378579@qq.com>
2022-08-01 16:33:32 +0800

    feat(data): 添加data api

--- src/api/business/data.ts
+++ src/api/business/data.ts
@@ -1,15 +1,55 @@
 import { defHttp } from '/@/utils/http/axios';
-import { getDataListResultModel } from './model/dataModel';
+import { getDataListResultModel, getOptionItemResultModel } from './model/dataModel';
+import { useBusinessStore } from '/@/store/modules/business';
+const useBusiness = useBusinessStore();
 
 enum Api {
-  // getDataList = '/api/dataList',
   getDataList = '/api/dataList',
+  getBoutiqueStation = '/api/boutiqueStation',
+  getDataTypeList = '/api/dataTypeList',
+  getDataTwoTypeList = '/api/dataTwoTypeList',
+  getSiteDataType = '/api/siteDataType',
+  dataDelete = '/api/dataDelete',
 }
 
 /**
- * @description: Get user menu based on id
+ * @description: Get data list based on params
  */
 
 export const getDataList = (params) => {
   return defHttp.get<getDataListResultModel>({ url: Api.getDataList, params });
+};
+/**
+ * @description: Get boutiqueStation list based
+ */
+export const getBoutiqueStation = async () => {
+  const data = await defHttp.post<getOptionItemResultModel>({ url: Api.getBoutiqueStation });
+  useBusiness.setBusinessOptions('boutique_station_id', data.items);
+  return data.items;
+};
+/**
+ * @description: Get getDataTypeList list based
+ */
+export const getDataTypeList = async () => {
+  const data = await defHttp.get<getOptionItemResultModel>({ url: Api.getDataTypeList });
+  useBusiness.setBusinessOptions('data_type_id', data.items);
+  return data.items;
+};
+/**
+ * @description: Get getDataTypeList list based
+ */
+export const getDataTwoTypeList = (params) => {
+  return defHttp.get<getOptionItemResultModel>({ url: Api.getDataTwoTypeList, params });
+};
+/**
+ * @description: Get getSiteDataType list based
+ */
+export const getSiteDataType = () => {
+  return defHttp.get<getOptionItemResultModel>({ url: Api.getSiteDataType });
+};
+/**
+ * @description: del info by id
+ */
+export const dataDelete = (params) => {
+  return defHttp.get<getOptionItemResultModel>({ url: Api.dataDelete, params });
 };
